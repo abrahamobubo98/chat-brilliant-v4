63fe3153e721eff61519c392685938c10126d232
abrahamobubo98 <84512669+abrahamobubo98@users.noreply.github.com>
2025-01-16 03:49:12 -0600

    image upload feature added

---
+++ src/features/upload/api/use-generate-upload-url.ts
@@ -0,0 +1,58 @@
+import { useMutation } from "convex/react";
+import { useCallback, useState, useMemo } from "react";
+
+import { api } from "../../../../convex/_generated/api";
+
+type ResponseType = string | null;
+
+type options = {
+    onSuccess?: (data: ResponseType) => void;
+    onError?: (error: Error) => void;
+    onSettled?: () => void;
+    throwError?: boolean;
+};
+
+export const useGenerateUploadUrl = (options?: options) => {
+    const [ data, setData ] = useState<ResponseType>(null);
+    const [ error, setError ] = useState<Error | null>(null);
+    const [ status, setStatus ] = useState<"success" | "error" | "settled" | "pending" | null>(null);
+
+    const isPending = useMemo(() => status === "pending", [status]);
+    const isSuccess = useMemo(() => status === "success", [status]);
+    const isError = useMemo(() => status === "error", [status]);
+    const isSettled = useMemo(() => status === "settled", [status]);
+
+    const mutation = useMutation(api.upload.generateUploadUrl);
+
+    const mutate = useCallback(async (values: {}, options?: options) => {
+        try{
+            setData(null);
+            setError(null);
+            setStatus("pending");
+
+            const response = await mutation();
+            options?.onSuccess?.(response);
+            return response;
+        } catch(error) {
+            setStatus("error");
+            options?.onError?.(error as Error);
+            if(options?.throwError) {
+                throw error;
+            }
+        } finally {
+            setStatus(null);
+            setStatus("settled");
+            options?.onSettled?.();
+        }
+    }, [mutation]);
+
+    return { 
+        mutate,
+        data,
+        error,
+        isPending,
+        isSuccess,
+        isError,
+        isSettled
+     };
+};
